c9da4ad681eed2e2a81438f52ec92095431e952b
MLC <vin_mlc@126.com>
2019-12-08 21:12:28 +0800

    scratchmaker

--- scratchmaker.ts
+++ scratchmaker.ts
@@ -1,22 +1,12 @@
 /**
- * Functions are mapped to blocks using various macros 
- * in comments starting with % (e.g., //% block).
- * The most important macro "block" specifies that a
- * block should be generated for a **exported** function.
+ * ScratchMaker graphics blocks
  */
-//% color="#AA278D"
+//% weight=100 color=#0f9c11 icon="\uf188"
 namespace scratchmaker{
 
-    //% block
+    //% blockId=scratchMakerHelloWorld block="HelloWorld"
     export function helloWorld() {
 
     }
 
-    // note that Caml casing yields lower case
-    // block text with spaces
-
-    //% block
-    export function camlCaseTwo() {
-
-    }
 }
